43f6b2ac3b75231eea1a914f46fb98b27e79c68d
cjclark <cjclark@wakehealth.edu>
2021-05-25 22:26:09 -0400

    inital commit

---
+++ ICD9to10crosswalk.py
@@ -0,0 +1,263 @@
+import pandas as pd
+import os
+import re
+import csv
+import datetime
+
+
+# ICD9 and ICD10 GEMS .txt files
+i9_cm_source_path = r'gem_files\2018_I9_cm_gem.txt'
+i10_cm_source_path = r'gem_files\2018_I10_cm_gem.txt'
+i9_pcs_source_path = r'gem_files\2018_I9_pcs_gem.txt'
+i10_pcs_source_path = r'gem_files\2018_I10_pcs_gem.txt'
+
+
+class ICD_CM_Conversion:
+
+    def __init__(self, i9_cm_path = i9_cm_source_path, i10_cm_path = i10_cm_source_path):
+        # cm
+        self.i9_cm_path = i9_cm_path
+        self.i10_cm_path = i10_cm_path
+        self.df9_cm= self.get_data(self.i9_cm_path, ['I9', 'I10', 'FLAG'])
+        self.df10_cm = self.get_data(self.i10_cm_path, ['I10', 'I9', 'FLAG'])
+
+    def get_data(self, path, cols):
+        """
+        Creates dataframe using ICD GEM text file.
+        :param path: path to ICD GEM text file
+        :param cols: column names as list
+        :return: dataframe
+        """
+        reg = re.compile(r'(\w*) +(\w*) +(\w*)')
+        with open(path) as file:
+            reader = csv.reader(file)
+            return pd.DataFrame([list(reg.search(str(i)).groups()) for i in reader], columns=cols)
+
+    def dict_append(self, base, new):
+        """
+        Returns base dictionary containing data structure as item, with appended items of new dictionary to base dictionary
+        :param base:
+        :param new:
+        :return: dtype(dict)
+        """
+        for i in new:
+            if i not in base.keys():
+                base[i] = new[i]
+                continue
+            else:
+                base[i].extend(new[i])
+                base[i] = list(set(base[i]))
+        return base
+
+    def icd9_to_10_cm(self, los):
+        """
+        Forward match from I9 GEMS to I10 GEMS
+        :param los:
+        :return: dtype(dict)
+        """
+        a = dict()
+
+        for i in los:
+            if i in a.keys():
+                a[i].extend(self.df9_cm[self.df9_cm['I9']==i]['I10'].values)
+                a[i] = list(set(a[i]))
+                continue
+            else:
+                a[i] = list(self.df9_cm[self.df9_cm['I9']==i]['I10'].values)
+        return a
+
+    def icd10_to_9_cm(self, list_of_codes):
+        """
+        Backward matchfrom I10 GEMs to I9 GEMS
+        :param list_of_codes:
+        :return: dtype(dict)
+        """
+        a = dict()
+
+        for i in list_of_codes:
+            if i in a.keys():
+                a[i].extend(list(self.df10_cm[self.df10_cm['I9']==i]['I10'].values))
+                a[i] = list(set(a[i]))
+            else:
+                a[i] = list(self.df10_cm[self.df10_cm['I9']==i]['I10'].values)
+        return a
+
+    def fwb_cm(self, list_of_codes):
+        """
+        Combined result of forward and backward matching
+        :param list_of_codes:
+        :return: dtype(dict)
+        """
+        f = self.icd9_to_10_cm(list_of_codes)
+        b = self.icd10_to_9_cm(list_of_codes)
+        return self.dict_append(f, b)
+
+    def sm_cm(self, list_of_codes):
+        """
+        Secondary matching, where secondary ICD9 codes are identified, and provided a forward backward match as well.
+        Seconary ICD9 codes are identified as other ICD9 codes that share ICD-10 codes, either through forward or
+        backward matching.
+        :param list_of_codes: source code file
+        :return: dtype(dict)
+        """
+        fwb_1 = self.fwb_cm(list_of_codes)
+        secondary = list()
+        # forward match of secondary
+        secondary.extend(list(set(self.df9_cm[self.df9_cm['I10'].isin([i for x in fwb_1.values() for i in x])]['I9'])))
+        # backward match of secondary
+        secondary.extend(list(set(self.df10_cm[self.df10_cm['I10'].isin([i for x in fwb_1.values() for i in x])]['I9'])))
+        print('Secondary ICD-9-cm_source codes: {}'.format(secondary))
+
+        return self.dict_append(fwb_1, self.fwb_cm(secondary))
+
+
+class ICD_PCS_Conversion:
+    def __init__(self, i9_pcs_path = i9_pcs_source_path, i10_pcs_path = i10_pcs_source_path):
+
+        # pcs
+        self.i9_pcs_path = i9_pcs_path
+        self.i10_pcs_path = i10_pcs_path
+        self.df9_pcs = self.get_data(self.i9_pcs_path, ['I9', 'I10', 'FLAG'])
+        self.df10_pcs = self.get_data(self.i10_pcs_path, ['I10', 'I9', 'FLAG'])
+
+    def get_data(self, path, cols):
+        """
+        Creates dataframe using ICD GEM text file.
+        :param path: path to ICD GEM text file
+        :param cols: column names as list
+        :return: dataframe
+        """
+        reg = re.compile(r'(\w*) +(\w*) +(\w*)')
+        with open(path) as file:
+            reader = csv.reader(file)
+            return pd.DataFrame([list(reg.search(str(i)).groups()) for i in reader], columns=cols)
+
+    def dict_append(self, base, new):
+        """
+        Returns base dictionary containing data structure as item, with appended items of new dictionary to base dictionary
+        :param base:
+        :param new:
+        :return: dtype(dict)
+        """
+        for i in new:
+            if i not in base.keys():
+                base[i] = new[i]
+                continue
+            else:
+                base[i].extend(new[i])
+                base[i] = list(set(base[i]))
+        return base
+
+    def icd9_to_10_pcs(self, los):
+        """
+        Forward match from I9 GEMS to I10 GEMS
+        :param los:
+        :return: dtype(dict)
+        """
+        a = dict()
+
+        for i in los:
+            if i in a.keys():
+                a[i].extend(self.df9_pcs[self.df9_pcs['I9']==i]['I10'].values)
+                a[i] = list(set(a[i]))
+                continue
+            else:
+                a[i] = list(self.df9_pcs[self.df9_pcs['I9']==i]['I10'].values)
+        return a
+
+    def icd10_to_9_pcs(self, list_of_codes):
+        """
+        Backward matchfrom I10 GEMs to I9 GEMS
+        :param list_of_codes:
+        :return: dtype(dict)
+        """
+        a = dict()
+
+        for i in list_of_codes:
+            if i in a.keys():
+                a[i].extend(list(self.df10_pcs[self.df10_pcs['I9']==i]['I10'].values))
+                a[i] = list(set(a[i]))
+            else:
+                a[i] = list(self.df10_pcs[self.df10_pcs,['I9']==i]['I10'].values)
+        return a
+
+    def fwb_pcs(self, list_of_codes):
+        """
+        Combined result of forward and backward matching
+        :param list_of_codes:
+        :return: dtype(dict)
+        """
+        f = self.icd9_to_10_pcs(list_of_codes)
+        b = self.icd10_to_9_pcs(list_of_codes)
+        return self.dict_append(f, b)
+
+    def sm_pcs(self, list_of_codes):
+        """
+        Secondary matching, where secondary ICD9 codes are identified, and provided a forward backward match as well.
+        Seconary ICD9 codes are identified as other ICD9 codes that share ICD-10 codes, either through forward or
+        backward matching.
+        :param list_of_codes: source code file
+        :return: dtype(dict)
+        """
+        fwb_1 = self.fwb_pcs(list_of_codes)
+        secondary = list()
+        # forward match of secondary
+        secondary.extend(list(set(self.df9_pcs[self.df9_pcs['I10'].isin([i for x in fwb_1.values() for i in x])]['I9'])))
+        # backward match of secondary
+        secondary.extend(list(set(self.df10_pcs[self.df10_pcs['I10'].isin([i for x in fwb_1.values() for i in x])]['I9'])))
+        print('Secondary ICD-9-CM Codes: {}'.format(secondary))
+
+        return self.dict_append(fwb_1, self.fwb_pcs(secondary))
+
+
+def read_files(rootdir, gem_type):
+    for subdir, dirs, files in os.walk(rootdir):
+        print(f'Number of Files: {len(files)}')
+        dfs = []
+        for file in files:
+            # print os.path.join(subdir, file)
+            filepath = subdir + os.sep + file
+            filename = file
+            current_time = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S')
+            df = process_file(filepath, gem_type)
+            # save data to file
+            df.to_csv(f'{current_time}_{filename}.csv', index=False)
+
+
+def process_file(data_file, gem_type):
+    print(data_file)
+    code_file = open(data_file, 'r')
+
+    list_of_codes = []
+    for line in code_file:
+        stripped_line = line.strip()
+        list_of_codes.append(stripped_line)
+
+    code_file.close()
+
+    icd_cm_converter = ICD_CM_Conversion()
+    icd_pcs_converter = ICD_PCS_Conversion()
+
+    if gem_type == 'cm':
+        converted_data = icd_cm_converter.icd9_to_10_cm(list_of_codes)
+    elif gem_type == 'pcs':
+        converted_data = icd_pcs_converter.icd9_to_10_pcs(list_of_codes)
+    else:
+        expanded = pd.DataFrame()
+        return expanded
+
+    df = pd.DataFrame.from_dict(converted_data.items())
+    df.rename(columns={0: 'ICD9', 1: 'ICD10'}, inplace=True)
+    expanded_df = df.explode('ICD10')
+
+    return expanded_df
+
+
+def main():
+    print('Start mapping...')
+    read_files(r'C:\Users\cjclark\Dropbox\Apps\FunctionLab\DECADES\ICD-9-cm_source', gem_type='cm')
+    read_files(r'C:\Users\cjclark\Dropbox\Apps\FunctionLab\DECADES\ICD-9-pcs_source', gem_type='pcs')
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,75 @@
+# ICD-9 to ICD-10 Crosswalk (Conversion, Mapping)
+Performs crosswalk mapping to and from ICD-9 to ICD-10.  
+
+##Background
+Crosswalk is based on CMS general equivalence mappings (GEMs).
+Diagnosis (CM) and procedure (PCS) codes are mapped.
+
+The purpose of the GEMs is to create a useful, practical, 
+code to code translation reference dictionary for both code 
+sets, and to offer acceptable translation alternatives wherever 
+possible. For each code set, it endeavors to answer this question: 
+Taking the complete meaning of a code (defined as: all correctly 
+coded conditions or procedures that would be classified to a code 
+based on the code title, all associated tabular instructional notes, 
+and all index references that refer to a code) as a single unit, what 
+are the most appropriate translation(s) to the other code set?
+
+##Mapping
+Mappings from specific concepts to more general concepts are possible; 
+however, it is not possible to use mappings to add specificity when 
+the original information is general.
+
+##Flags
+####1st Flag
+The GEM provides an approximate flag (1st Flag).  The approximate flag identifies 
+entries where the complete meaning of the source system code and that 
+of the target system code are not considered equivalent.  
+
+The approximate flag is 0 when the complete meaning of the source and the 
+target are considered equivalent and the source and target translate only 
+to each other in both GEMs files.
+
+When the approximate flag is 1 the complete meaning of the source and the 
+target are not considered equivalent.
+
+####2nd Flag
+The no map flag distinguishes entries where the source system code has at 
+least one translation from entries where the source system code has no 
+target system translation. Every effort is made to find an acceptable 
+translation in the target system for every code in the source system. 
+The no map flag is used only as a last resort, when there are no acceptable 
+target system translation alternatives for the source system code.
+
+####3rd Flag
+The combination flag distinguishes entries where the source system code has 
+a single (meaning “non-combination”) translation alternative(s) from entries 
+where the source system has a combination alternative(s).
+
+####4th Flag
+A source system combination code includes diagnostic conditions or procedures 
+that require more than one separate code in the target system to convey the 
+equivalent amount of information. A combination code may also describe multiple 
+variations of the information in either the code title or the complete meaning 
+of the code. Each of these variations has its own number in the scenario field 
+(the 4th flag).
+
+####5th Flag
+Choice lists (the 5th flag) are the method of organization for the translation 
+alternatives in a combination entry. Choice lists organize the distinct 
+components of the target system translation in a combination entry 
+into pick lists.
+
+## Input file format
+51884  
+99666  
+51881  
+V5877  
+73025  
+
+##Sources:
+https://www.cms.gov/Medicare/Coding/ICD10/2018-ICD-10-PCS-and-GEMs
+https://www.cms.gov/Medicare/Coding/ICD10/2018-ICD-10-CM-and-GEMs
+https://github.com/Ed-Nonnenmacher/ICD9-ICD10.git
+https://github.com/bhanratt/ICD9CMtoICD10CM.git
+Last Accessed: 5/25/2017
